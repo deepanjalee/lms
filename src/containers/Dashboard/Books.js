2c94f3663c8e6d54f6ee64038aaef227ff1c0609
deepanjalee <deepanjalee.s.k27@gmail.com>
2021-09-26 16:20:48 +0530

    object null issue fix

--- src/containers/Dashboard/Books.js
+++ src/containers/Dashboard/Books.js
@@ -6,31 +6,31 @@ import { FluidContainer } from "../../components/CommonComponent";
 
 const Books = ({ catelog }) => {
 
-    const upadteCategory = [...catelog,
-    {
-        author: "Rowling",
-        burrowedDate: "",
-        burrowedMemberId: "",
-        id: "3",
-        isAvailable: true,
-        title: "Harry",
-    },
-    {
-        author: "Rowling",
-        burrowedDate: "",
-        burrowedMemberId: "",
-        id: "3",
-        isAvailable: false,
-        title: "Harry",
-    }
-    ]
+    // const upadteCategory = [...catelog,
+    // {
+    //     author: "Rowling",
+    //     burrowedDate: "",
+    //     burrowedMemberId: "",
+    //     id: "3",
+    //     isAvailable: true,
+    //     title: "Harry",
+    // },
+    // {
+    //     author: "Rowling",
+    //     burrowedDate: "",
+    //     burrowedMemberId: "",
+    //     id: "3",
+    //     isAvailable: false,
+    //     title: "Harry",
+    // }
+    // ]
 
     const handleTableRowClick = (id) => {
         console.log(id);
     }
     return (
         <FluidContainer>
-            <Table data={upadteCategory} handleRowClick={handleTableRowClick} instruction="Please click the book row for display the more details" />
+            <Table data={catelog} handleRowClick={handleTableRowClick} instruction="Please click the book row for display the more details" />
 
         </FluidContainer>
     );
--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -37,7 +37,7 @@ const Dashboard = () => {
     ];
 
     return (
-        isLoading ? <Spinner /> : <Tabs contents={contents} />
+        isLoading ? <Spinner /> :books.length > 0 && <Tabs contents={contents} /> 
     )
 
 
